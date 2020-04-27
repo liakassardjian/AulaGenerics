80eae3be6dd1e478e575aed79aa36d0f4df88a3c
liakassardjian <liakassardjian@gmail.com>
2020-04-27 17:54:43 -0300

    Correção do último exercício e do bônus

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -44,24 +44,48 @@ array.sumAll()
 
 // MARK: - Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
 
-func countElements(first: Array<Any>, second: Array<Any>) -> Int {
-    return first.count + second.count
+func countElements<T: Equatable>(first: Array<T>, second: Array<T>) -> Int {
+    var count = 0
+    for element in first {
+        if second.contains(element) {
+            count += 1
+        }
+    }
+
+    return count
 }
 
-let newArray = ["a", "b", "c", "d", "e"]
+let newArray = [1, 2, 3, 4, 5]
 countElements(first: array, second: newArray)
 
 
 // MARK: - BONUS: Fazer o método acima receber N arrays
 
-func countAnyElements(_ arrays: Array<Any>...) -> Int {
-    var count = 0
-    for array in arrays {
-        count += array.count
+// A função só considera se um elemento for encontrado em todos os arrays
+func countAnyElements<T: Equatable>(_ arrays: Array<T>...) -> Int {
+    var copy = arrays
+    let firstArray = copy.removeFirst()
+    
+    var results = [T]()
+    
+    for element in firstArray {
+        var isContainedIn = 1
+        
+        for array in copy {
+            if !array.contains(element) {
+                break
+            }
+            
+            isContainedIn += 1
+        }
+        
+        if isContainedIn == arrays.count && !results.contains(element) {
+            results.append(element)
+        }
     }
     
-    return count
+    return results.count
 }
 
-let anotherNewArray = [1.25, 1.5, 2.75, 3.55]
+let anotherNewArray = [1, 2, 3, 4]
 countAnyElements(array, newArray, anotherNewArray)
