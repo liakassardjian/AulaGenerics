2d90493549c1648e370176d16b8b06beb7ed0911
liakassardjian <liakassardjian@gmail.com>
2020-04-27 17:19:49 -0300

    Exercícios

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -17,7 +17,51 @@ var pilha = Pilha<Int>()
 pilha.push(intA)
 pilha.push(intB)
 
-//TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
-//TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
-//TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
-    //BONUS TODO: Fazer o método acima receber N arrays
+// MARK: - Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+
+func contains<T: Equatable>(array: Array<T>, element: T) -> Bool {
+    return array.contains(element)
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+contains(array: array, element: 5)
+
+
+// MARK: - Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
+
+extension Array where Element: Numeric {
+    func sumAll() -> Element {
+        var sum = Element.zero
+        for element in array {
+            sum += Element(exactly: element) ?? 0
+        }
+        return sum
+    }
+}
+
+array.sumAll()
+
+
+// MARK: - Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
+
+func countElements(first: Array<Any>, second: Array<Any>) -> Int {
+    return first.count + second.count
+}
+
+let newArray = ["a", "b", "c", "d", "e"]
+countElements(first: array, second: newArray)
+
+
+// MARK: - BONUS: Fazer o método acima receber N arrays
+
+func countAnyElements(_ arrays: Array<Any>...) -> Int {
+    var count = 0
+    for array in arrays {
+        count += array.count
+    }
+    
+    return count
+}
+
+let anotherNewArray = [1.25, 1.5, 2.75, 3.55]
+countAnyElements(array, newArray, anotherNewArray)
